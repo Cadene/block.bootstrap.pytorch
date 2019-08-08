097356da4f0b311c1c7302496568ccfb771ad326
Remi <remi.cadene@icloud.com>
2019-08-08 00:55:56 -0700

    fix

--- block/datasets/scripts/download_vqa2.sh
+++ block/datasets/scripts/download_vqa2.sh
@@ -5,6 +5,7 @@ wget http://data.lip6.fr/cadene/block/coco.tar.gz
 tar -xzvf vqa2.tar.gz
 tar -xzvf coco.tar.gz
 
+cd ../..
 mkdir -p data/vqa/coco/extract_rcnn
 cd data/vqa/coco/extract_rcnn
 wget http://data.lip6.fr/cadene/block/coco/extract_rcnn/2018-04-27_bottom-up-attention_fixed_36.tar
