0d182d0a0fd33567a6ce6c1b844ded0c8465ec2f
Remi <remi.cadene@icloud.com>
2019-09-09 09:28:12 -0700

    Update download_vgenome.sh

--- block/datasets/scripts/download_vgenome.sh
+++ block/datasets/scripts/download_vgenome.sh
@@ -3,7 +3,7 @@ cd data/vqa
 wget http://data.lip6.fr/cadene/block/vgenome.tar.gz
 tar -xzvf vgenome.tar.gz
 
-mkdir -p data/vqa/vgenome/extract_rcnn
-cd data/vqa/vgenome/extract_rcnn
+mkdir -p vgenome/extract_rcnn
+cd vgenome/extract_rcnn
 wget http://data.lip6.fr/cadene/block/vgenome/extract_rcnn/2018-04-27_bottom-up-attention_fixed_36.tar
 tar -xvf 2018-04-27_bottom-up-attention_fixed_36.tar
